87a37d3f9689b93661f72994b80db60ad8f4e5a6
Jim Barry <JBarry@Data-Strategy.com>
2018-12-07 11:53:19 -0500

    added copycells function

--- ExcelReports.py
+++ ExcelReports.py
@@ -11,23 +11,34 @@ Create form in Tkinter:
 """
 
 
-print("Hello Jim")
 wb = openpyxl.load_workbook(
         r'\\dsfiles01\Managed Services\GEMS Reporting\2018\rawFiles\SGS UCDay2_MSOpenandCompletedTickets.xlsx'
     )
 sheetnames = wb.get_sheet_names()
-print(sheetnames)
-sheet = wb.get_sheet_by_name(sheetnames[0])
-print(sheet)
-rowcount = 0
-for row in range(1, sheet.max_row+1):
-    if sheet['A'+str(row)].value == 'Ticket Number':
-        print("found it!")
-        print(row)
-        rowarray = []
-        rowarray += row
-        print(rowarray)
-        rowcount += 1
+ws = wb.get_sheet_by_name(sheetnames[0])
+
+wsrows = ws.max_row
+wscols = ws.max_column
+print("wsrows " + str(wsrows))
+print("wscols " + str(wscols))
+
+def copycells(startrow, endrow, startcol, endcol, sheet):
+    rangesel = []
+    for i in range(startrow, endrow + 1, 1):
+        rowsel = []
+        for j in range(startcol, endcol + 1, 1):
+            rowsel.append(ws.cell(row=i, column=j).value)
+        rangesel.append(rowsel)
+
+    return rangesel
+
+
+def findtext(s_row, e_row, s_col, e_col, searchsheet, searchtext):
+    cell_info = copycells(s_row, e_row, s_col, e_col, searchsheet)
+    for c in cell_info:
+        print(cell_info[c][1].value)
+
+findtext(1, wsrows, 1, wscols, ws, "Ticket Number")
 
 
 mainWindow = tk.Tk()
