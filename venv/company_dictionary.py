9f19c3d36aecba933015e4b6a2d638f253be94e2
jbarry1506 <jbarry1506@gmail.com>
2018-12-19 09:31:14 -0500

    First GitHub commit

---
+++ venv/company_dictionary.py
@@ -0,0 +1,237 @@
+email_lists = {
+    "ACBL": {
+        "To": [
+            'James.Trendel@aclines.com; ',
+            'Josh.Esterle@aclines.com; ',
+        ],
+        "CC": [
+            'bob.fehder@data-strategy.com; ',
+        ],
+    },
+    "American Fuji Seal": {
+        "To": [
+        ],
+        "CC": [
+        ],
+    },
+    "Arby's": {
+        "To": [
+        ],
+        "CC": [
+        ],
+    },
+    "Big Ass Solutions": {
+        "To": [
+            'wade.mcdaniel@bigassfans.com; ',
+            'tarianto@bigasssolutions.com; ',
+        ],
+        "CC": [
+            'John.Burkhart@data-strategy.com; ',
+        ],
+    },
+    "Braidy Industries": {
+        "To": [
+            'mrakes@braidyindustries.com; ',
+        ],
+        "CC": [
+            'John.Burkhart@data-strategy.com; ',
+        ],
+    },
+    "Brandstetter Carroll": {
+        "To": [
+            'cschneider@brandstettercarroll.com; ',
+        ],
+        "CC": [
+            'john.burkhart@data-strategy.com; ',
+        ],
+    },
+    "Cafe Press": {
+        "To": [
+            'cmartinho@cafepress.com; ',
+        ],
+        "CC": [
+            'Steve.Willis@data-strategy.com; ',
+        ],
+    },
+    "Communicare": {
+        "To": [
+            'RParrott@communicare.org; ',
+            'smosqueda@communicare.org; ',
+        ],
+        "CC": [
+            'Steve.Willis@data-strategy.com; ',
+        ],
+    },
+    "eBridge": {
+        "To": [
+            'margie.ragland@ebridgeglobal.com; ',
+        ],
+        "CC": [
+            'Dave.Shahroudi@data-strategy.com; ',
+        ],
+    },
+    "Empire": {
+        "To": [
+            'johnsonj@empireco.com; ',
+        ],
+        "CC": [
+            'emily.przekadzinski@data-strategy.com; ',
+        ],
+    },
+    "Franklin Electric": {
+        "To": [
+            'evan.hunter@fele.com; ',
+        ],
+        "CC": [
+            'Aaron.Leonard@data-strategy.com; ',
+        ],
+    },
+    "FKNC": {
+        "To": [
+            'kstilts@fortknoxnational.com; ',
+        ],
+        "CC": [
+            'Bob.Fehder@data-strategy.com; ',
+        ],
+    },
+    "ISCO": {
+        "To": [
+            'kim.byram@isco-pipe.com; ',
+            'tj.duncan@isco-pipe.com; ',
+            'Kevin.west@isco-pipe.com; ',
+            'mickey.smith@isco-pipe.com; ',
+        ],
+        "CC": [
+            'Nick.Pozaric@data-strategy.com',
+        ],
+    },
+    "MainGate": {
+        "To": [
+            'thenion@maingateinc.com; ',
+            'tkabrich@maingateinc.com; ',
+        ],
+        "CC": [
+            'Bradyn.Bennett@data-strategy.com; ',
+        ],
+    },
+    "Mary Free Bed": {
+        "To": [
+        ],
+        "CC": [
+        ],
+    },
+    "Mercer": {
+        "To": [
+            'rbyers@mercer-trans.com; ',
+            'adezayee@mercer-trans.com; ',
+        ],
+        "CC": [
+            'Steve.Willis@data-strategy.com; ',
+        ],
+    },
+    "RiteCom": {
+        "To": [
+            'cpenny@ritecomgroup.com; ',
+        ],
+        "CC": [
+            'jhoving@optiodata.com; ',
+        ],
+    },
+    "Royal": {
+        "To": [
+            'french.williams@royaltechnologies.com; ',
+        ],
+        "CC": [
+            'Alex.Peschel@data-strategy.com; ',
+        ],
+    },
+    "RQSI": {
+        "To": [
+            'jyounie@rqsi.com, chall@rqsi.com; ',
+        ],
+        "CC": [
+            'Steve.Willis@data-strategy.com; ',
+        ],
+    },
+    "RSCS": {
+        "To": [
+        ],
+        "CC": [
+        ],
+    },
+    "Sazerac": {
+        "To": [
+            'tcolliver@sazerac.com; ',
+            'jbrown@sazerac.com; ',
+            'dnguyen@sazerac.com; ',
+        ],
+        "CC": [
+            'Matt.Poynter@data-strategy.com; ',
+        ],
+    },
+    "SGS": {
+        "To": [
+            'Mike.Mahoney@sgsco.com; ',
+            'Brian.Densford@sgsco.com; ',
+        ],
+        "CC": [
+            'Bob.Fehder@data-strategy.com; ',
+        ],
+    },
+    "SGS-UC Day2": {
+        "To": [
+            'Mike.Mahoney@sgsco.com; ',
+            'Brian.Densford@sgsco.com; ',
+        ],
+        "CC": [
+            'Bob.Fehder@data-strategy.com; ',
+        ],
+    },
+    "Spalding": {
+        "To": [
+            'apeach@spalding.edu; ',
+            'EKrumhansl@spalding.edu; ',
+        ],
+        "CC": [
+            'Dave.Shahroudi@data-strategy.com; ',
+        ],
+    },
+    "Spelman": {
+        "To": [
+        ],
+        "CC": [
+        ],
+    },
+    "The Oliver Group": {
+        "To": [
+            'eolson@olivergroup.com; ',
+        ],
+        "CC": [
+            'Steve.Willis@data-strategy.com; ',
+        ],
+    },
+    "Town Square Bank": {
+        "To": [
+            'bworkman@townsquarebank.com; ',
+        ],
+        "CC": [
+            'John.Burkhart@data-strategy.com; ',
+        ],
+    },
+    "UFP": {
+        "To": [
+            'jbjork@ufpi.com; ',
+        ],
+        "CC": [
+            'aj.heyboer@data-strategy.com; ',
+        ],
+    },
+    "WHA": {
+        "To": [
+            'mhall@isapin.com; ',
+        ],
+        "CC": [
+            'Aaron.Leonard@data-strategy.com; ',
+        ],
+    },
+}
